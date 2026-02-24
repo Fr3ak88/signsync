a06226731fb137586e154e31f4c48a79cf0ffb13
Fr3ak88 <42094080+Fr3ak88@users.noreply.github.com>
2026-02-24 14:12:20 +0100

    Add initial web routes in web.php

---
+++ signsync/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ZeiteintragController;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/',
