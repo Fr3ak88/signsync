772ec6ddf45996c4fde056e370a34a3de7bf857e
Fr3ak88 <42094080+Fr3ak88@users.noreply.github.com>
2026-03-19 12:16:54 +0100

    Delete signsync1111 directory

--- signsync1111/app/http/controllers/PdfExportController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Http\Controllers;
-
-use App\Models\Zeiteintrag;
-use Illuminate\Http\Request;
-use Barryvdh\DomPDF\Facade\Pdf;
-
-class
--- signsync1111/resources/views/dashboard.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-3">
-            <div class="card bg
