607d580962cbf48dd6f79fea808450f18ce42e54
Fr3ak88 <42094080+Fr3ak88@users.noreply.github.com>
2026-05-05 07:59:39 +0000

    1

--- app/Models/Schueler.php
+++ app/Models/Schueler.php
@@ -23,19 +23,5 @@ protected $casts = [
     'birth_date' => 'date', 
 ];
 
-public function getNameAttribute($value)
-    {
-        if (empty($value)) return $value;
 
-        try {
-            // Laravel hat durch das $casts['name' => 'encrypted'] 
-            // den Wert eigentlich schon entschlüsselt. 
-            // Falls es aber knallt, fangen wir es hier ab.
-            return decrypt($value);
-        } catch (\Exception $e) {
-            // Falls der Wert schon Klartext ist (wie deine jetzigen Testdaten)
-            // oder der Key falsch ist, gib den Rohwert zurück.
-            return $value;
-        }
-    }
 }
